6f50a99bae9eb34eb80327503f3984dc18e1e370
Afaq Ashfaq <afaqashfaq@live.com>
2020-11-23 12:44:03 +0500

    Implemented Login

---
+++ ShopperOnline/wwwroot/js/APIURI.js
@@ -0,0 +1,6 @@
+﻿//FACEBOOK DEV ACCOUNT APP ID
+const fbAppId = '357684835582932'
+
+//RAPID API KEY
+const rapidApiKey = "bd9fbd2bd1msh27bbb1cfb3648f9p1af4a1jsn0415a6279879"
+const rapidApiHost = "shopalive.p.rapidapi.com"
