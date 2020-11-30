ffedeb54e29500c0325d87fc10c36ecdeda6530b
kashifkhanniazi <kashifkhanniazi45@gmail.com>
2020-11-30 17:02:24 +0500

    working on showpagelist

---
+++ ShopperOnline/Models/ViewModel/PageList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopperOnline.Models.ViewModel
+{
+    public class PageList
+    {
+        public string Id { get; set; }
+        public string AccessToken { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+    }
+}
--- ShopperOnline/wwwroot/js/APIURI.js
+++ ShopperOnline/wwwroot/js/APIURI.js
@@ -1,5 +1,5 @@
 ﻿//FACEBOOK DEV ACCOUNT APP ID
-const fbAppId = '357684835582932'
+const fbAppId = '660396818182883'
 
 //RAPID API KEY
 const rapidApiKey = "bd9fbd2bd1msh27bbb1cfb3648f9p1af4a1jsn0415a6279879"
